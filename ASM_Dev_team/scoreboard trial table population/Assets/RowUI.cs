272a3d26b9da8a9e4408b85282f507615559824f
Siva Thangavel <siva0004@student.monash.edu>
2023-04-23 13:13:05 +1000

    research work for row population on unity from c# script

---
+++ ASM_Dev_team/scoreboard trial table population/Assets/RowUI.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RowUI : MonoBehaviour
+{
+    public Text rank;
+    public Text name;
+    public Text score;
+}
