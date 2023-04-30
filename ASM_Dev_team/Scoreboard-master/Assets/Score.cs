29a4e43e7937a9ca8d13e0dbdf006ea8e6a2dad7
Xiaoyi Weng <xwen0022@student.monash.edu>
2023-04-30 22:59:55 +1000

    Luke added documentation as requested by Siva

--- ASM_Dev_team/Scoreboard-master/Assets/Score.cs
+++ ASM_Dev_team/Scoreboard-master/Assets/Score.cs
@@ -1,14 +1,24 @@
-﻿using System;
+﻿// The 'using' keyword imports the System namespace, which contains the Serializable attribute.
+using System;
 
+// This line defines a new public class called Score.
+// The [Serializable] attribute indicates that instances of this class can be serialized and deserialized.
 [Serializable]
 public class Score
 {
+    // These two public fields are used to store the name and score of a player's score.
     public string name;
     public float score;
 
+    // This is the constructor for the Score class.
+    // It takes in two parameters: a string for the player's name and a float for their score.
     public Score(string name, float score)
     {
+        // This line assigns the 'name' parameter to the 'name' field of the Score instance being constructed.
         this.name = name;
+
+        // This line assigns the 'score' parameter to the 'score' field of the Score instance being constructed.
         this.score = score;
     }
 }
+
--- ASM_Dev_team/Scoreboard-master/Assets/ScoreUi.cs
+++ ASM_Dev_team/Scoreboard-master/Assets/ScoreUi.cs
@@ -1,16 +1,19 @@
-﻿using System.Collections;
+﻿// The 'using' keyword imports the System.Collections, System.Collections.Generic, System.Linq, UnityEngine, and System namespaces.
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using System;
 
+// This line defines a new public class called ScoreUi that inherits from MonoBehaviour.
 public class ScoreUi : MonoBehaviour
 {
+    // These public fields are used to reference a TextAsset object that contains the score data, a ScoreManager object, and a RowUi object.
     public TextAsset textAssetData;
     public ScoreManager scoreManager;   
     public RowUi rowUi;
     
- 
+    // These [Serializable] classes are used to deserialize the score data from the TextAsset object.
     [System.Serializable]
     public class Country
     {
@@ -24,17 +27,25 @@ public class ScoreUi : MonoBehaviour
         public Country[] country; 
     }
 
+    // This public field holds an instance of the CountryList class, which is used to store the score data.
     public CountryList myCountryList = new CountryList();
 
+    // This method is called when the script instance is being loaded.
     void Start()
     {
+        // Load the score data from the TextAsset object.
         textAssetData = Resources.Load<TextAsset>("data");
 
+        // Split the score data into an array of strings using ',' and '\n' as delimiters.
         string[] data = textAssetData.text.Split(new string[] { ",", "\n" }, StringSplitOptions.None);
 
+        // Calculate the size of the score data table.
         int tablesize = data.Length / 3 - 1;
+
+        // Initialize the Country array in the CountryList with the calculated table size.
         myCountryList.country = new Country[tablesize];
 
+        // Populate the Country array in the CountryList with the score data.
         for (int i = 0; i < tablesize; i++)
         {
             myCountryList.country[i] = new Country();
@@ -43,12 +54,13 @@ public class ScoreUi : MonoBehaviour
             myCountryList.country[i].score = int.Parse(data[3 * (i + 1) + 2]);
         }
 
+        // Add the score data to the ScoreManager.
         for (int i = 0; i < tablesize; i++)
         {
             scoreManager.AddScore(new Score(myCountryList.country[i].name, myCountryList.country[i].score));
         }
         
-        
+        // Retrieve the high scores from the ScoreManager and display them in the UI.
         var scores = scoreManager.GetHighScores().ToArray();
         for (int i = 0; i < scores.Length; i++)
         {
@@ -59,4 +71,5 @@ public class ScoreUi : MonoBehaviour
         }
     }
 }
+
 
