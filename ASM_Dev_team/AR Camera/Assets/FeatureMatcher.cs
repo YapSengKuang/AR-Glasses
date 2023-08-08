b27b281b5fe0618f21a678de2c103dbba5a2e2b6
Aayush Jay Sarma <asar003@student.monash.edu>
2023-08-08 12:32:09 +1000

    reverting to jennie's branch

---
+++ ASM_Dev_team/AR Camera/Assets/FeatureMatcher.cs
@@ -0,0 +1,28 @@
+﻿// FeatureMatcher.cs
+using Emgu.CV;
+using Emgu.CV.Util;
+
+public class FeatureMatcher
+{
+    public static VectorOfDMatch Match(Mat modelDescriptors, Mat queryDescriptors)
+    {
+        using (var matcher = new BFMatcher(DistanceType.L2))
+        {
+            VectorOfVectorOfDMatch matches = new VectorOfVectorOfDMatch();
+            matcher.Add(modelDescriptors);
+            matcher.KnnMatch(queryDescriptors, matches, 2, null);
+
+            VectorOfDMatch goodMatches = new VectorOfDMatch();
+            for (int i = 0; i < matches.Size; i++)
+            {
+                VectorOfDMatch match = matches[i];
+                if (match.Size >= 2 && match[0].Distance < 0.75 * match[1].Distance)
+                {
+                    goodMatches.Push(new DMatch[] { match[0] });
+                }
+            }
+
+            return goodMatches;
+        }
+    }
+}
---
+++ ASM_Dev_team/AR Camera/Assets/Main.cs
@@ -0,0 +1,12 @@
+﻿string modelImagePath = "...";
+string queryImagePath = "...";
+
+Mat modelImage = ImagePreprocessor.Preprocess(modelImagePath);
+(VectorOfKeyPoint modelKeyPoints, Mat modelDescriptors) = FeatureExtractor.ExtractFeatures(modelImage);
+
+Mat queryImage = ImagePreprocessor.Preprocess(queryImagePath);
+(VectorOfKeyPoint queryKeyPoints, Mat queryDescriptors) = FeatureExtractor.ExtractFeatures(queryImage);
+
+VectorOfDMatch matches = FeatureMatcher.Match(modelDescriptors, queryDescriptors);
+
+float score = ResultSorter.SortAndReturn(matches);
